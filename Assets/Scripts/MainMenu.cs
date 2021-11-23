47b3e74c2d11a6f2962ba9fde0926174687ebd76
Simon van Leent <s.j.vanleent@student.tudelft.nl>
2021-11-23 13:23:27 +0100

    added login menu
    added a login menu and scripts for it

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,4 +15,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("ExampleScene");
     }
+
+    public void LoginPage()
+    {
+        SceneManager.LoadScene("Authenticate");
+    }
 }
