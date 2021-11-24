08397868e253af4f838cdf293ee628db6aa2ff58
Simon van Leent <s.j.vanleent@student.tudelft.nl>
2021-11-24 14:52:37 +0100

    Added working playername
     usable over multiple scenes, using login scene and main menu scene.

--- Assets/Scripts/Authenticate.cs
+++ Assets/Scripts/Authenticate.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Authenticate : MonoBehaviour
 {
+    //private string input;
+    public InputField playername;
+
     public void ExitToMenu()
     {
+        MainMenu.playernamestr = playername.text;
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("player name is:" + playername.text);
     }
+
+    //public void SetPlayerName()
+    //{
+        //player_name = UsernameInput.text;
+        //Debug.Log(player_name);
+        
+        //input = s;
+        //Debug.Log(input);
+
+    //}
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    //public TextMeshProUGUI display_player_name;
+
+    public static string playernamestr;
+
+    public Text playername;
+
+    void Start()
+    {
+        playername.text = playernamestr;
+    }
+
     public void ExitButton()
     {
         Application.Quit();
