e7d988183a2df777d56b824af308411a70fe3aa7
Simon van Leent <s.j.vanleent@student.tudelft.nl>
2021-12-01 13:50:51 +0100

    Started on Powerup system
    Added a random powerup generator based on if the player lost the game before.

--- Assets/Scripts/RoundMenu.cs
+++ Assets/Scripts/RoundMenu.cs
@@ -2,11 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class RoundMenu : MonoBehaviour
 {
     public static string playernamestr;
     public static string stagestr;
+    public static bool wonlastgamebool;
+    public Text earnedpowerup;
+    public Text playerscore;
+    public static string wonpowerup;
+
+    void Start()
+    {
+
+        //wonpowerup = "you won powerup1";
+        Pupicker();
+        wonlastgamebool = true;
+        if (wonlastgamebool == true)
+        {
+            earnedpowerup.text = wonpowerup;
+
+        }
+        if (wonlastgamebool == false)
+        {
+            earnedpowerup.text = "you lost...";
+        }
+    }
+
+    static void Pupicker()
+    {
+        var random = new System.Random();
+        var list = new List<string> { "BigMass", "Invisibility", "GhostFlyer", "FastSpeed", "JumpBoost" };
+        int index = random.Next(list.Count);
+        wonpowerup = list[index];
+    }
 
     public void ExitToMenu()
     {
