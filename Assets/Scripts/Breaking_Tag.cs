dbfddea8c3d7dd5b1e30f0258a5fc8783200c0ce
Vincent Goossens <v.a.goossens@student.tudelft.nl>
2021-12-03 23:50:02 +0100

    Added a 2nd stage
    -Stage introduces a new kind of platform, that breaks x (public variable) seconds after touching it and then respawns x seconds later

---
+++ Assets/Scripts/Breaking_Tag.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breaking_Tag : MonoBehaviour
+{
+
+    public float delayTime = 2.0f;
+    private bool enabled = true;
+
+    IEnumerator OnTriggerEnter(Collider coll) {
+    if(enabled == true){  
+
+        enabled = false;
+        Debug.Log("yep");
+    
+        yield return new WaitForSeconds(delayTime);
+        
+        foreach(Transform child in gameObject.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        
+        
+        yield return new WaitForSeconds(delayTime);
+        enabled = true; 
+        foreach(Transform child in gameObject.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+     }
+    }
+
+
+     
+}
