d772e274bd4c5e69fcbe3fe6493f48002435b5b9
Aleksander Sliwinski <A.J.Sliwinski@student.tudelft.nl>
2021-12-06 02:40:55 +0100

    Attack script and player script
    Attack script:
    -sends damage value to other player
    -attack direction vector (on collision with other player)
    -Calculates collision vector
    -added/updated variables
    
    Player script:
    -player receives damage upon attack
    -player gets forced applied when attacked by other player
    -added/updated variables

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -6,10 +6,12 @@ public class AttackScript : MonoBehaviour
 {
 
     public float damage = 15.0F;
-    public float attackDuration = 0.3F;
+    public float attackDuration = 1F;
     public bool attacking = false;
+    public float attackStrength = 1F;
 
-    [HideInInspector]
+
+
 
     // Notes:
     // - I need to make so you cant attack when you have been hit --> for some x duration 
@@ -21,26 +23,45 @@ public class AttackScript : MonoBehaviour
 
     void Update()
     {
-
+        #region attacks
         // If x attack key is being pressed, attacking state initialized to True
+        // We need to map attack variables to any attack
         if (Input.GetKeyDown("x"))
         {
-            //attackdirection = some attack vector
-            //attackduration = depends on attack
+            // z is horizontal direction
+            // y is vertical direction
+            //public Vector3 attackimpulse = new Vector3(5.0f, 3.0f, 5.0f);
+            GetComponent<Animation>().Play("ANIMATON_NAME");
+            //Animation.animatePhysics //Necessary to be true for animation to be run alongside physics (so collisions)
+            damage = 5;
+            attackDuration = 1;
             attacking = true;
         }
+#endregion  
+
+
+
     }
 
-    // If player collides with trigger object then receivedamage method is called on the trigger
-    void OnTriggerEnter(Collider col)
+    // If player collides with trigger object then receive damage method is called on the trigger
+    void OnTriggerEnter(Collider other)
     {
         // We need to setup tag
-        if (col.tag == "Player")
+        if (other.tag == "Player")
         {
             if (attacking)
             {
-                col.SendMessage("receiveDamage", damage, SendMessageOptions.DontRequireReceiver);
-                //col.SendMessage("knockback", attackdirection, SendMessageOptions.DontRequireReceiver);
+                //Calculate collision vector
+                var attackDirection = transform.position - other.transform.position;
+
+
+                // normalize force vector to only get direction + get rid of x component 
+                attackDirection.Normalize();
+                attackDirection.x = 0;
+
+                //Send funcitons calls
+                other.SendMessage("ReceiveDamage", damage, SendMessageOptions.DontRequireReceiver);
+                other.SendMessage("Knockback", attackDirection, SendMessageOptions.DontRequireReceiver);
             }
         }
     }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    #region Properties and variabes
+    #region Properties and variabes;
 
 
     [SerializeField] public float StartingHealth = 0;
@@ -13,10 +14,14 @@ public class PlayerScript : MonoBehaviour
     public float DamageMultiplier = 1;  //could vary by class or with powerups
 
     public float knockbackMultiplier = 1; //Could vary by class 
-
+    private float x;
     public Text HealthText;
 
     public float health;
+
+    private Rigidbody rb;
+
+    
 
     #endregion
 
@@ -27,6 +32,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody>();
         health = StartingHealth;
 
         //health = StartingHealth;
@@ -45,17 +51,30 @@ public class PlayerScript : MonoBehaviour
 
 
 
-    public void receiveDamage(float damage)
+    public void ReceiveDamage(float damage)
     {
 
         health += damage * DamageMultiplier;
         Debug.Log("Damage Applied");
     }
 
-
-    public void knockback(Vector3 attackdirection)
+    //
+    public void Knockback(Vector3 attackDirection )
     {
+        //converting from double to float (necessary for vector operations)
 
+        x = (float)Math.Pow(health, 0.5);
+
+        //Scale vector magnitude by damage percent taken by character
+        attackDirection.z *= x;
+        attackDirection.y *= x;
+
+        //attackDirection.z *=  (float)Math.Pow(health,0.5) ;
+        //attackDirection.y *= (float)Math.Pow(health, 0.5);
+        attackDirection.x *= 0;
+
+        //Applying force to rigidbody
+        rb.AddForce(attackDirection, ForceMode.Impulse);
     }
 
 
