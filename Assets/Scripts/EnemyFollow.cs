56428e6e728ae5d2c65102a46fe84e3343c1f26b
Simon van Leent <s.j.vanleent@student.tudelft.nl>
2021-12-13 09:53:04 +0100

    AI basic attacks
    The AI can attack the player with basic attacks. The player can not attack the AI back yet.

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyFollow : MonoBehaviour
 {
@@ -9,10 +10,32 @@ public class EnemyFollow : MonoBehaviour
     public Transform player;
     private bool inRange = false;
 
+    public GameObject characterOBJ;
+    public Text HealthText;
+    public float defense = 100;
+    private float damageTaken = 0;
+    private float knockbackScalar = 1;
+    public float knockPercent;
+
+    private bool isBlocking = false;
+    private bool isPunching = false;
+    private bool isKicking = false;
+
+    private int punchDamage = 15;
+    private int kickDamage = 50;
+
+    public Collider[] attackHitboxes;
+    Animator controllerANIM;
+    private Rigidbody rb;
+
+    float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateHealth();
+        controllerANIM = characterOBJ.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -28,6 +51,9 @@ public class EnemyFollow : MonoBehaviour
         {
             inRange = true;
             Debug.Log("in range = true");
+            isPunching = true;
+            StartCoroutine("Cooldown");
+
         }
     }
 
@@ -38,5 +64,92 @@ public class EnemyFollow : MonoBehaviour
             inRange = false;
             Debug.Log("in range = false");
         }
+    }
+
+    void TakeDamage(int attackDamage)
+    {
+        damageTaken += attackDamage;
+        rb.AddForce(new Vector3(0, 0.5f, 2) * 5 * knockbackScalar, ForceMode.Impulse);
+        UpdateHealth();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isPunching)
+        {
+            isBlocking = false;
+            isKicking = false;
+            isPunching = false;
+            controllerANIM.SetTrigger("Punch");
+            controllerANIM.SetBool("Block", isBlocking);
+            launchAttack(attackHitboxes[0], punchDamage);
+
+        }
+        else if (isKicking)
+        {
+            isBlocking = false;
+            isKicking = false;
+            isPunching = false;
+            controllerANIM.SetTrigger("Kick");
+            controllerANIM.SetBool("Block", isBlocking);
+            launchAttack(attackHitboxes[1], kickDamage);
+        }
+    }
+
+    void UpdateHealth()
+    {
+        knockPercent = damageTaken / defense;
+        knockbackScalar = 1 + knockPercent / 4;
+        HealthText.text = (knockPercent * 100).ToString("0") + "%";
+        Debug.Log((knockPercent * 100).ToString("0") + "%");
+    }
+
+    //public void Punch(InputAction.CallbackContext context)
+    //{
+    //    Debug.Log("Punch!");
+    //    if (context.performed)
+    //    {
+    //        isPunching = true;
+    //    }
+    //    else
+    //    {
+    //        isPunching = false;
+    //    }
+    //}
+
+    //public void Kick(InputAction.CallbackContext context)
+    //{
+    //    Debug.Log("Punch!");
+    //    if (context.performed)
+    //    {
+    //        isKicking = true;
+    //    }
+    //    else
+    //    {
+    //        isKicking = false;
+    //    }
+    //}
+
+    //public void Block(InputAction.CallbackContext context)
+    //{
+    //}
+
+    void launchAttack(Collider col, int attackDamage)
+    {
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hitbox"));
+        foreach (Collider c in cols)
+        {
+            if (c.transform.root == transform)
+            {
+                continue;
+            }
+
+            c.SendMessageUpwards("TakeDamage", attackDamage);
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
