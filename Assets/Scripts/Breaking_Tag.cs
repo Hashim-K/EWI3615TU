c8dd2232e7e3df7ddaf0d1bf6dcf8388c9859bad
Simon van Leent <s.j.vanleent@student.tudelft.nl>
2022-01-09 15:54:21 +0100

    AI can now fall of the stages
    When colliding with the AI it will turn off, if you hit it afterwards, it can fall of the stages. Also it can fall of the breaking platforms now.

--- Assets/Scripts/Breaking_Tag.cs
+++ Assets/Scripts/Breaking_Tag.cs
@@ -18,25 +18,26 @@ public class Breaking_Tag : MonoBehaviour
     
         yield return new WaitForSeconds(delayTime);
 
-        //GetComponent<NavMeshAgent>().enabled = false;
+        
         //Debug.Log("navmeshagent is disabled breaking");
         
         foreach (Transform child in gameObject.transform)
         {
             child.gameObject.SetActive(false);
         }
-        
+        GetComponent<NavMeshAgent>().enabled = false;
         
         yield return new WaitForSeconds(delayTime);
         enabled = true;
         
-        //GetComponent<NavMeshAgent>().enabled = true;
+        
         //Debug.Log("navmeshagent is enabled breaking");
         foreach (Transform child in gameObject.transform)
         {
             child.gameObject.SetActive(true);
         }
-     }
+        GetComponent<NavMeshAgent>().enabled = true;
+        }
     }
 
 
